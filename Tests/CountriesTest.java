46452b428460cb9a0baf7c35562310d5a61bbdaf
Ikbal Aslan <ikball.aslann@gmail.com>
2022-12-25 19:27:07 +0300

    Upd-1

---
+++ Tests/CountriesTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CountriesTest {
+     /*
+        Given
+          User connects to the database
+​
+        When
+          User sends the query to get the region ids from "countries" table
+​
+        Then
+          Verify that the number of region ids greater than 1 is 17.
+​
+        And
+          User closes the connection
+       */
+    @Test
+    public void countryTest(){
+        //User connects to the database
+        JdbcUtils.connectToDatabase("localhost","Techpro","postgres","aslan4646");
+        JdbcUtils.createStatement();
+       // User sends the query to get the region ids from "countries" table
+        String sql = "SELECT region_id FROM countries";
+        //Verify that the number of region ids greater than 1 is 17.
+        List<Object> region_ids = JdbcUtils.getColumnList1("region_id","countries");
+
+        //2.Way
+       int idsGreaterThan1 = region_ids.stream().filter(t->(Integer)t>1).collect(Collectors.toList()).size();
+        System.out.println(idsGreaterThan1);
+        assertEquals(17,idsGreaterThan1);
+
+
+
+    }
+
+}
